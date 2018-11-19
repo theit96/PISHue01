7e854feb5a084819f928815ab88217552ebb844c
Thomas Heit <thomas.heit@mni.thm.de>
2018-11-19 17:58:17 +0100

    GUI drin, Package geändert

---
+++ src/pis/hue01/CodecGUI.java
@@ -0,0 +1,23 @@
+package pis.hue01;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+
+
+
+public class CodecGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/pis/hue01/Test.java
@@ -0,0 +1,8 @@
+package pis.hue01;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
